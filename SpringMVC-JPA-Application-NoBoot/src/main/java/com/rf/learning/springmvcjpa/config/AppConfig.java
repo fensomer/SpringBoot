de08e1a535a5bbd13bd87d62e79711ec1334f685
Reece Fensome <FensomeR@pcms.local>
2017-11-03 22:18:47 +0000

    Add configuration and implementation.

--- SpringMVC-JPA-Application-NoBoot/src/main/java/com/rf/learning/springmvcjpa/config/AppConfig.java
+++ SpringMVC-JPA-Application-NoBoot/src/main/java/com/rf/learning/springmvcjpa/config/AppConfig.java
@@ -1,15 +1,98 @@
 package com.rf.learning.springmvcjpa.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "com.rf.learning.springmvcjpa.repos")
+@EnableJpaRepositories(basePackages = "com.rf.learning.springmvcjpa.repositories")
 @PropertySource(value = {"classpath:application.properties"})
 public class AppConfig
 {
+  @Autowired
+  private Environment env;
 
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer placeholderConfigurer()
+  {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager()
+  {
+    EntityManagerFactory factory = entityManagerFactory().getObject();
+    return new JpaTransactionManager(factory);
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory()
+  {
+    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+
+    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    vendorAdapter.setShowSql(Boolean.TRUE);
+
+    factory.setDataSource(dataSource());
+    factory.setJpaVendorAdapter(vendorAdapter);
+    factory.setPackagesToScan(env.getProperty("packages.to.scan"));
+
+    Properties jpaProperties = new Properties();
+    jpaProperties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+    jpaProperties.put("show_sql", env.getProperty("hibernate.show_sql"));
+    jpaProperties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+    factory.setJpaProperties(jpaProperties);
+
+    factory.afterPropertiesSet();
+    factory.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+    return factory;
+  }
+
+  @Bean
+  public HibernateExceptionTranslator hibernateExceptionTranslator()
+  {
+    return new HibernateExceptionTranslator();
+  }
+
+  @Bean
+  public DataSource dataSource()
+  {
+    BasicDataSource dataSource = new BasicDataSource();
+    dataSource.setDriverClassName(env.getProperty("jdbc.driver.className"));
+    dataSource.setUrl(env.getProperty("jdbc.url"));
+    dataSource.setUsername(env.getProperty("jdbc.username"));
+    dataSource.setPassword(env.getProperty("jdbc.password"));
+    return dataSource;
+  }
+
+  @Bean
+  public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+    DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+    dataSourceInitializer.setDataSource(dataSource);
+    ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+    databasePopulator.addScript(new ClassPathResource(env.getProperty("db.init.script")));
+    dataSourceInitializer.setDatabasePopulator(databasePopulator);
+    dataSourceInitializer.setEnabled(Boolean.parseBoolean(env.getProperty("db.init.enabled", "false")));
+    return dataSourceInitializer;
+  }
 }
