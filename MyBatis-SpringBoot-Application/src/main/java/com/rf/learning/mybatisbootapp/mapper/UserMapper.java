977c9b695b0b2f9603ffe4e6d2757c087b2cbd9a
Reece Fensome <reece.fensome@pcmsgroup.com>
2017-11-08 14:36:01 +0000

    Add MyBatis Spring Boot project.

---
+++ MyBatis-SpringBoot-Application/src/main/java/com/rf/learning/mybatisbootapp/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.rf.learning.mybatisbootapp.mapper;
+
+import com.rf.learning.mybatisbootapp.domain.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper
+{
+  @Select("select id, name, email from users")
+  List<User> findAllUsers();
+
+  @Select("select id, name, email from users where id=#{id}")
+  User findUserById(Integer id);
+
+  @Insert("insert into users(name,email) values(#{name},#{email})")
+  @SelectKey(statement = "call identity()", keyProperty = "id", before = false, resultType = Integer.class)
+  void insertUser(User user);
+}
---
+++ MyBatis-SpringBoot-Application/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USERS (id, name, email) VALUES (1, 'John', 'john@gmail.com');
+INSERT INTO USERS (id, name, email) VALUES (2, 'Rod', 'rod@gmail.com');
+INSERT INTO USERS (id, name, email) VALUES (3, 'Mike', 'mike@gmail.com');
